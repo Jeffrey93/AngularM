e1d962cb89233dcce43d86ee81dc5cdbdf5a6f38
Jeffrey93 <ingenierojeffry@gmail.com>
2017-08-23 11:54:00 -0500

    router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,22 @@ import { AppComponent } from './app.component';
 import { MaterializeModule } from 'angular2-materialize';
 import { AgmCoreModule } from '@agm/core';
 import { LoginComponent } from './login/login.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+ /*
+  {
+    path: 'heroes',
+    component: HeroListComponent,
+    data: { title: 'Heroes List' }
+  },
+  { path: '',
+    redirectTo: '/heroes',
+    pathMatch: 'full'
+  }/*,
+  { path: '**', component: PageNotFoundComponent }*/
+];
 
 @NgModule({
   declarations: [
@@ -13,7 +29,13 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
+    //habilitar modulo route
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     MaterializeModule,
+    //importar apikey para mapas
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyAcNmYmk-E19uEqsArE0CqQEkWE7VapnZ8'
     })
